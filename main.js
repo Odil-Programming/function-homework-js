aa4964d7745bbd49f57d648fe8278ebf6ef4ad43
Odil-Programming <odilchikfurqatovich@gmail.com>
2024-04-04 13:51:09 +0500

    function

---
+++ main.js
@@ -0,0 +1,6 @@
+function showMax(a, b, c, d, e) {
+    alert(Math.max(a, b, c, d, e));
+}
+
+// Funksiyani sinab ko'rish uchun misol:
+showMax(5, 10, 65, 29, 199);  // Alert orqali 10 raqamini ko'rsatadi
